3194aa53800f4eedc2f92b5ea4528568c070e557
NourOumy <STG0088@BRUFOR.LAN>
2024-01-29 16:08:07 +0100

    push fin de journée

--- scripts/main.js
+++ scripts/main.js
@@ -24,7 +24,7 @@ function addition() {
 
 // addition() => rien ne s'affiche ici si on ne faitt pas de console.log
 
-// Arguments de la fonction :
+// Arguments de la fonction (dès qu'on a identifié ce qui va varier d'un utilisateur à l'autre = argument):
 
 // ici, on laisse le choix à l'utilisateur de mettre ce qu'il veut à la place de nbr1 et nbr2
 
@@ -39,6 +39,7 @@ function addition() {
 let addname = document.querySelector (".nom")
 let coucou = document.querySelector(".coucou")
 let button = document.querySelector("button")
+let result = document.querySelector(".result")
 
 function salutations(prenom){
     if(addname.value != ""){
@@ -50,4 +51,65 @@ function salutations(prenom){
 
 button.addEventListener("click", function(){
     salutations(addname.value)
+})
+
+
+// 2.Comparer deux nombres et afficher si le premier est plus grand que le second (ou s'il est égal), les nombres doivent être envoyés par 2 champs input de type nombre et la fonction doit se lancer au click sur un bouton de validation
+
+let compare = document.querySelector(".compare")
+let input1 = document.querySelector(".input1")
+let input2 = document.querySelector(".input2")
+let result2 = document.querySelector(".result2")
+
+function comparaison(nbr1, nbr2){
+    if(nbr1>nbr2){
+       result2.innerHTML = `${nbr1} est supérieur à ${nbr2}`
+    } else if(nbr1<nbr2){
+       result2.innerHTML = `${nbr1} est inférieur à ${nbr2}`
+    } else {
+       result2.innerHTML = `${nbr1} est égal à ${nbr2}`
+    }
+nbr1.value == ""
+nbr2.value == ""
+    }
+    
+compare.addEventListener("click", function(){
+    comparaison(parseInt(input1.value),parseInt(input2.value))
+    
+})
+
+// 3. Demandez à l'utilisateur son poids (en kg) et sa taille (en m) et calculez son IMC
+
+let calculerImc = document.querySelector(".calculerImc")
+let kg = document.querySelector(".poids")
+let metre = document.querySelector(".taille")
+let resultImc = document.querySelector(".resultImc")
+let resultatImc = 0
+
+function imc(poids,taille){
+    resultatImc= poids/taille**2
+    if(resultatImc<18,5){
+        resultImc.innerHTML = `Votre IMC est de ${resultatImc} ce qui signifie que vous êtes en insuffisance pondérale (maigreur)`
+    }
+    if(resultatImc<25){
+        resultImc.innerHTML = `Votre IMC est de ${resultatImc} ce qui signifie que vous êtes de corpulence normale`
+    }
+    if(resultatImc<30){
+        resultImc.innerHTML = `Votre IMC est de ${resultatImc} ce qui signifie que vous êtes en surpoids`
+    }
+    if(resultatImc<35){
+        resultImc.innerHTML = `Votre IMC est de ${resultatImc} ce qui signifie que vous êtes en obésité modérée`
+    }
+    if(resultatImc<40){
+        resultImc.innerHTML = `Votre IMC est de ${resultatImc} ce qui signifie que vous êtes en obésité sévère`
+    }
+    if(resultatImc>40){
+        resultImc.innerHTML = `Votre IMC est de ${resultatImc} ce qui signifie que vous êtes en obésité morbide`
+    }
+
+}
+
+calculerImc.addEventListener("click", function(){
+    imc(parseInt(kg.value), parseInt(metre.value))
+    
 })
